46238a8b22eec95900a01277270d2f04860108d9
willemhscott <willem.scott@colorado.edu>
2021-01-31 17:18:05 -0700

    Fixes to movement

--- games/aigame/round.py
+++ games/aigame/round.py
@@ -63,12 +63,58 @@ class GameRound(object):
         target.health -= actor.damage
         actor.attacked_this_round = True
 
-    def dispatch_move(self, actor, pos):
+    @staticmethod
+    def raycast(start: tuple[int, 2], end: tuple[int, 2]) -> set[tuple]:
+        """
+        https://gamedev.stackexchange.com/questions/20103/finding-which-tiles-are-intersected-by-a-line-without-looping-through-all-of-th
+
+        :param start: Start position
+        :param end: End position
+        :return: List of intersecting tiles
+        """
+        x0, y0 = start
+        x1, y1 = end
+        dx = abs(x1 - x0)
+        dy = abs(y1 - y0)
+        x = x0
+        y = y0
+        ns = 1 + dx + dy
+        x_inc = 1 if (x1 > x0) else -1
+        y_inc = 1 if (y1 > y0) else -1
+        dx *= 2
+        dy *= 2
+        error = dx - dy
+
+        tiles = set()
+
+        for n in range(ns, 0, -1):
+            tiles.add((x,y))
+            if error > 0:
+                x += x_inc
+                error -= dy
+            elif error < 0:
+                y += y_inc
+                error += dx
+            elif error == 0:
+                x += x_inc
+                y += y_inc
+                error -= dy
+                error += dx
+
+        return tiles
+
+    def dispatch_move(self, actor: 'units.Unit', pos: tuple[int, 2]):
+        if pos[0] < 0 or pos[1] < 0:
+            raise RuntimeError(f"Invalid coordinate pair {pos}")
+        if pos[0] >= len(self.game.map[0]) or pos[1] >= len(self.game.map[1]):
+            raise RuntimeError(f"Invalid x coordinate {pos[0]}")
+
         if actor.moved_this_round:
             raise RuntimeError(f"{actor} has already moved this round!")
 
-        if self.game.map[tuple(pos)] == 1:
-            raise RuntimeError(f"{actor} attempted invalid move to impassable tile")
+        for step in self.raycast(actor.position, pos):
+            if self.game.map[tuple(step)] == 1:
+                raise RuntimeError(f"{actor} attempted invalid move to or through impassable tile {step}")
 
         actor.move(pos)
         actor.moved_this_round = True
